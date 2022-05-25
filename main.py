fa2dcf788ae17807b7f9301698f6795850777f76
Barker Willison <105759611+borkar5@users.noreply.github.com>
2022-05-25 01:00:36 +0000

    built statement decorater
    half done first question
    intro semi done

---
+++ main.py
@@ -0,0 +1,54 @@
+#te reo quiz- 10 questions 
+
+#this function decorates the intro statement 
+def statement_generator(statement, decoration):
+
+    sides = decoration * 3
+
+    statement = "{} {} {}".format(sides, statement, sides)
+    top_bottom = decoration * len(statement)
+
+    print(top_bottom)
+    print(statement)
+    print(top_bottom)
+
+    return ""
+
+
+
+
+
+#these lines of code will start the quiz 
+
+
+  
+  
+  
+  
+  
+#these lines will be the questions 
+
+
+score = 0 
+def question_start():
+  print("Question 1:")
+  print("what is the maori word for car?")
+  print("A: kapua")
+  print("B: Waea")
+  print("C: Waka")
+  print("D: koromatua")
+  input ==
+  
+  
+
+
+  
+  
+  
+  
+
+  
+  
+  
+#these lines of code will greet the user and ask them weather they would like to play
+statement_generator("welcome to the te reo maori quiz", "#")
