751e1590c33ff1fad52e1d782bc3f257968be830
Barker Willison <105759611+borkar5@users.noreply.github.com>
2022-05-27 01:44:48 +0000

    added start quiz function built first question made intro question

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 #te reo quiz- 10 questions 
 
+
+
+#these lines of code will start the quiz 
+def quiz_stort():
+  print("have you played the te reo maori quiz before? ")
+  quiz_start = input().lower()
+  print(quiz_start)
+  if quiz_start == "yes" or quiz_start =="y":
+    question_start()
+  
+  elif quiz_start == "no" or quiz_start == "n":
+    show_instructions()
+  
+  else:   
+    print("please enter yes or no! ")
+    
+
+#this is the instructions function 
+def show_instructions():
+  print("instructions ")
+
+
 #this function decorates the intro statement 
 def statement_generator(statement, decoration):
 
@@ -13,42 +35,30 @@ def statement_generator(statement, decoration):
     print(top_bottom)
 
     return ""
-
-
-
-
-
-#these lines of code will start the quiz 
-
-
-  
-  
-  
-  
-  
+    
 #these lines will be the questions 
 
 
-score = 0 
+
 def question_start():
+  score = 0
   print("Question 1:")
   print("what is the maori word for car?")
   print("A: kapua")
   print("B: Waea")
   print("C: Waka")
   print("D: koromatua")
-  input ==
-  
-  
+  question_1 = input().lower
+  if question_1 == "a" or question_1 == "waka":
+    print("correct") 
+    score += 1
+    print("your score is {}".format(score))
+  else: 
+    print("incorrect")
+    score += 0
+    print("your score is {}".format(score))
 
-
-  
-  
-  
-  
-
-  
-  
-  
 #these lines of code will greet the user and ask them weather they would like to play
 statement_generator("welcome to the te reo maori quiz", "#")
+
+quiz_stort()
